3f06dc8c4ab4dbcf6e1561d2efd2a5e73aa1d18c
Nadhif Lazuardi <berkasnadhif@gmail.com>
2024-09-21 18:46:46 +0700

    first commit

---
+++ src/components/NotesApp.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import { getInitialData, showFormattedDate } from "../utils/data";
+
+class ContactApp extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: getInitialData(),
+            dates: showFormattedDate(new Date()),
+        };
+
+        this.onDeleteHandler = this.onDeleteHandler.bind(this);
+        this.onAddContactHandler = this.onAddContactHandler.bind(this);
+    }
+
+    onDeleteHandler(id) {
+        const contacts = this.state.contacts.filter((contact) => contact.id !== id);
+        this.setState({ contacts });
+    }
+
+    onAddContactHandler({ name, tag }) {
+        this.setState((prevState) => {
+            return {
+                contacts: [
+                    ...prevState.contacts,
+                    {
+                        id: +new Date(),
+                        name,
+                        tag,
+                        imageUrl: "/images/default.jpg",
+                    },
+                ],
+            };
+        });
+    }
+
+    render() {
+        return (
+            <div className="contact-app">
+                <h1>Aplikasi Kontak</h1>
+                <h2>Tambah Kontak</h2>
+                {/* <ContactInput addContact={this.onAddContactHandler} /> */}
+                <h2>Daftar Kontak</h2>
+                {/* <ContactList contacts={this.state.contacts} onDelete={this.onDeleteHandler} /> */}
+            </div>
+        );
+    }
+}
+
+export default ContactApp;
